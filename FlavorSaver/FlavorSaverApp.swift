4b4b85d1e93879125b11f66b172806519a74e53c
Jacky Gao <jackygao02@andrew.cmu.edu>
2023-10-29 15:55:58 -0400

    Initial Commit

---
+++ FlavorSaver/FlavorSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorSaverApp.swift
+//  FlavorSaver
+//
+//  Created by Jacky Gao on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FlavorSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
