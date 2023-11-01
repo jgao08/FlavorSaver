9d753076efdeca245dd579bf3f201ce472c67cde
Jennifer Zhang <jlz2@andrew.cmu.edu>
2023-11-01 18:49:58 -0400

    basic mechanics of SearchView

--- FlavorSaver/Views/SearchView.swift
+++ FlavorSaver/Views/SearchView.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct SearchView: View{
+  let ingredients = ["apple pie", "apple", "artichoke"]
+  @State private var selectedIngs = ["almond", "banana"]
+  @State private var searchText = ""
+
+  var body: some View {
+    VStack{
+//    MARK: Top bar including selected ingredients and Done button
+      HStack{
+        Spacer()
+        ForEach(selectedIngs, id: \.self){ ingredient in
+          Button(action: {
+          }, label: {
+            HStack{
+              Text(ingredient)
+              Image(systemName: "xmark").onTapGesture {
+                if let index = selectedIngs.firstIndex(where:{$0 == ingredient}){
+                  selectedIngs.remove(at: index)
+                }
+              }
+            }
+          })
+          .buttonStyle(.bordered)
+        }
+        Spacer()
+        Button("Done",
+          action: {})
+          .buttonStyle(.borderedProminent)
+          .frame(alignment: .trailing)
+        Spacer()
+      }
+      
+//    MARK: Searched ingredient list
+      NavigationView{
+        List{
+          ForEach(ingredients, id: \.self){ ingredient in
+            Button(action: {
+              selectedIngs.append(ingredient)
+            }){
+              Text(ingredient)
+            }
+          }
+        }
+      }.searchable(text: $searchText)
+    }
+  }
+}
+
+#Preview {
+  SearchView()
+}
