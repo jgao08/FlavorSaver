4212d85a46c94c6cbb5a598b5fa4b97cc3053b3a
erica-w-fu <efu@andrew.cmu.edu>
2023-11-07 13:58:09 -0500

    added to cooking mode

--- FlavorSaver/Views/CookingModeView.swift
+++ FlavorSaver/Views/CookingModeView.swift
@@ -8,11 +8,80 @@
 import SwiftUI
 
 struct CookingModeView: View {
+    @State var recipe : Recipe
     var body: some View {
-        Text("Let me cook!")
+        TabView {
+            VStack {
+                ForEach(recipe.getRecipeSteps(), id: \.0){ step, ingredients in
+                    VStack{
+                        HStack{
+                            Text(step)
+                            Spacer()
+                        }
+                        HStack{
+                            VStack{
+                                ForEach(ingredients, id: \.self){ ingredient in
+                                    HStack{
+                                        Button(action: {}, label: {
+                                            Text(ingredient)
+                                        })
+                                        .disabled(true)
+                                        .buttonStyle(.bordered)
+                                        .foregroundStyle(Color.black)
+                                        Spacer()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                Spacer()
+            }
+        }
     }
 }
 
-#Preview {
-    CookingModeView()
+struct CookingModeIntro: View {
+    var body: some View {
+        
+        VStack (spacing: 128){
+            VStack (spacing: 64){
+                HStack {
+                    Text("Cooking Mode")
+                        .font(.headline)
+                    Spacer()
+                }
+                Text("Welcome to Cooking Mode!")
+                    .font(.title)
+                    .foregroundStyle(Color.gray)
+            }
+            HStack{
+                Spacer()
+                VStack (alignment: .trailing) {
+                    HStack {
+                        Text("To go to the next step")
+                            .font(.body)
+                        Image(systemName: "chevron.right")
+                    }
+                    Text("Swipe left")
+                        .font(.title)
+                }
+            }
+            
+            HStack {
+                VStack (alignment: .leading) {
+                    HStack {
+                        Image(systemName: "chevron.left")
+                        Text("To go to the previous step")
+                            .font(.body)
+                    }
+                    Text("Swipe right")
+                        .font(.title)
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+        .padding(.horizontal, 16)
+    }
 }
--- FlavorSaver/Views/RecipeCardSmall.swift
+++ FlavorSaver/Views/RecipeCardSmall.swift
@@ -12,59 +12,55 @@ import Foundation
 import SwiftUI
 
 struct RecipeCardSmall: View {
-//  @Binding var RecipeInfo: Recipe_Info
     var recipe: Recipe
-    
-//    let imageURL = URL(string: "https://spoonacular.com/recipeImages/633975-556x370.jpg")
-//    let recipeName = "Cheeseburger"
-//    let readyInMinutes = 50
-//    let cuisines = ["American, French"]
-//    let author = "Gordon Ramsay"
   
-  var body: some View  {
-    ZStack{
-        AsyncImage(url: URL(string: recipe.image))
-//            .renderingMode(.original)
-//            .resizable()
-//            .aspectRatio(contentMode: .fill)
-            .frame(width: 170, height: 230)
-            .clipped()
-            .cornerRadius(10)
-        
-        Rectangle()
-            .fill(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.1), .black.opacity(0.6)]), startPoint: .top, endPoint: .bottom))
-            .frame(width: 170, height: 230)
-            .cornerRadius(10)
-
-
-        HStack() {
-            VStack(alignment: .leading, spacing: 4) {
-                Text(recipe.name)
-                    .font(.headline)
-                    .fontWeight(.bold)
-                HStack(spacing: 4) {
-                    Text(String(recipe.readyInMinutes))
-                    Text("mins")
-                    Text("•")
-                    Text(recipe.cuisines.joined(separator: ", "))
-                        .lineLimit(1)
-                }
-                .font(.caption)
-                
-                Spacer()
-                
-                Text(recipe.author)
-                    .font(.caption)
-            }
-            .shadow(color: .black, radius: 8)
+    var body: some View  {
+        NavigationLink (destination: RecipeView(recipeSearch: recipe), label: {
             
-            Spacer()
-        }
-        .padding(16)
-        .foregroundStyle(.white)
-        .frame(width: 170, height: 230)
+            ZStack{
+                AsyncImage(url: URL(string: recipe.image))
+                //            .renderingMode(.original)
+                //            .resizable()
+                //            .aspectRatio(contentMode: .fill)
+                    .frame(width: 170, height: 230)
+                    .clipped()
+                    .cornerRadius(10)
+                
+                Rectangle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.black.opacity(0.1), .black.opacity(0.6)]), startPoint: .top, endPoint: .bottom))
+                    .frame(width: 170, height: 230)
+                    .cornerRadius(10)
+                
+                
+                HStack() {
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(recipe.name)
+                            .font(.headline)
+                            .fontWeight(.bold)
+                        HStack(spacing: 4) {
+                            Text(String(recipe.readyInMinutes))
+                            Text("mins")
+                            Text("•")
+                            Text(recipe.cuisines.joined(separator: ", "))
+                                .lineLimit(1)
+                        }
+                        .font(.caption)
+                        
+                        Spacer()
+                        
+                        Text(recipe.author)
+                            .font(.caption)
+                    }
+                    .shadow(color: .black, radius: 8)
+                    
+                    Spacer()
+                }
+                .padding(16)
+                .foregroundStyle(.white)
+                .frame(width: 170, height: 230)
+            }
+        })
     }
-  }
 }
 
 //#Preview {
