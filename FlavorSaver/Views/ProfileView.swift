37809c0f49335fbace4dc9907a2a1224cd82ea23
Jennifer Zhang <jlz2@andrew.cmu.edu>
2023-11-30 22:55:04 -0500

    created profile view

--- FlavorSaver/Views/ProfileView.swift
+++ FlavorSaver/Views/ProfileView.swift
@@ -10,15 +10,45 @@ import SwiftUI
 
 
 struct ProfileView: View {
+  @EnvironmentObject var user: User
+  
   var body: some View{
-    VStack{
-      Image("testimg")
-        .resizable()
-        .aspectRatio(contentMode: .fill)
-        .frame(width: 100, height: 100)
-        .clipShape(Circle())
-      Text("Steven Zhang")
-        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+    NavigationStack{
+      VStack{
+        Image("testimg")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 100, height: 100)
+          .clipShape(Circle())
+        Text("Steven Zhang")
+          .font(.title)
+          .bold()
+        Divider()
+        NavigationLink(destination: SavedRecipesView(), label: {
+          VStack{
+            HStack{
+              Text("Saved Recipes")
+                .font(.title)
+              Spacer()
+              Image(systemName: "chevron.right")
+                .font(.title)
+            }
+            .foregroundStyle(Color.black)
+            .padding()
+
+            HStack {
+                ScrollView (.horizontal, showsIndicators: false ){
+                    HStack{
+                      ForEach(user.getSavedRecipes(), id: \.self){ recipe in
+                            RecipeCardLarge(recipe: recipe)
+                        }
+                    }
+                }
+            }.padding(.horizontal)
+          }
+          
+        })
+      }
     }
   }
 }
